fd996052cad527e3781d436d96a69b0f4c994114
Davidroa763 <30906467+Davidroa763@users.noreply.github.com>
2017-09-04 22:20:32 -0500

    2
    w

--- Foot-AppWeb/src/app/app.module.ts
+++ Foot-AppWeb/src/app/app.module.ts
@@ -3,12 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {
+  MdAutocompleteModule, MdButtonModule, MdButtonToggleModule, MdPaginatorModule,
+  MdCardModule, MdCheckboxModule, MdChipsModule, MdDatepickerModule,
+  MdDialogModule, MdGridListModule, MdIconModule, MdInputModule,
+  MdListModule, MdMenuModule, MdProgressBarModule, MdProgressSpinnerModule,
+  MdRadioModule, MdSelectModule, MdSidenavModule, MdSliderModule, MdSortModule,
+  MdSlideToggleModule, MdSnackBarModule, MdTableModule, MdTabsModule, MdToolbarModule,
+  MdTooltipModule
+} from '@angular/material';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MdAutocompleteModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdChipsModule,
+    MdDatepickerModule,
+    MdDialogModule,
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdMenuModule,
+    MdProgressBarModule,
+    MdProgressSpinnerModule,
+    MdRadioModule,
+    MdSelectModule,
+    MdSlideToggleModule,
+    MdSliderModule,
+    MdSidenavModule,
+    MdSnackBarModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
+    MdPaginatorModule,
+    MdSortModule,
+    MdTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
